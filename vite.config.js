0108294c745025271d9fc6bab8ccb596f7696a8b
petershindp <ssm.peter.shin@gmail.com>
2025-05-08 19:51:44 -0700

    vite config base change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/peter-shin-dp/", // Set this to your repository name
 	plugins: [react()],
 	build: {
 		outDir: "build",

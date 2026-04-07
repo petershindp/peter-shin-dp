d987beaa23c6ff5b7ce3c23ebf573c635ad847a3
petershindp <ssm.peter.shin@gmail.com>
2026-04-07 14:48:53 -0700

    removed movie schema from sanity

--- peter-shin-dp-cms/schemaTypes/index.ts
+++ peter-shin-dp-cms/schemaTypes/index.ts
@@ -1,10 +1,8 @@
 import blockContent from './blockContent'
-import movie from './movie'
 import project from './project'
 import about from './about'
 export const schemaTypes = [
   // Document types
-  movie,
   project,
   about,
 
